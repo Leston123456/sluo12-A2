7fdfaf4b4dc3caee3f72ca8ad6b34aeb99145f3e
Leston123456 <162035340+Leston123456@users.noreply.github.com>
2025-11-21 21:12:20 +0800

    Create AssignmentTwo.java

---
+++ src/AssignmentTwo.java
@@ -0,0 +1,13 @@
+public class AssignmentTwo {
+    public static void main(String[] args) {
+        System.out.println("PROG2004 A2 基础代码初始化完成（Part1-Part2）");
+    }
+
+    // 后续模块演示方法（空实现）
+    public void partThree() {}
+    public void partFourA() {}
+    public void partFourB() {}
+    public void partFive() {}
+    public void partSix() {}
+    public void partSeven() {}
+}
